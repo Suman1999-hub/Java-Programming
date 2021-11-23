1066e359da52a2ced9f882e13cd26244983ad302
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2021-11-23 23:07:33 +0530

    Add files via upload

---
+++ IncreasingA.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+class IncreasingA{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		long sum=0;
+		for(int i=0;i<n-1;i++){
+			if(arr[i]>arr[i+1]){
+				sum +=arr[i]-arr[i+1];
+				arr[i+1]=arr[i];
+			}
+		}
+		System.out.print(sum);
+	}
+}
